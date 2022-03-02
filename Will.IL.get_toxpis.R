f5fca6026e51bc57a7d64e51c31a471a9bc28fc5
wachiuphd <34288126+wachiuphd@users.noreply.github.com>
2022-03-01 21:00:50 -0600

    Extracted Will County data

---
+++ Will.IL.get_toxpis.R
@@ -0,0 +1,21 @@
+library(data.table)
+indicators.df<-fread("CVI_indicators_current.csv")
+categories <- unique(indicators.df$`Baseline Vulnerability`)
+cvi.df<-fread("CVI_data_current.csv",
+              keepLeadingZeros = TRUE)
+indx.will.il <- cvi.df$STATE=="IL" & cvi.df$County_Name=="Will"
+
+for (i in 1:length(categories)) {
+  onecat <- categories[i]
+  cvi.pct.toxpi.cat<-fread(paste0("CVI-pct-cat-",gsub(": ","-",onecat),".csv"),
+                           keepLeadingZeros = TRUE)
+  will.il.cvi.pct.toxpi.cat <- cvi.pct.toxpi.cat[indx.will.il,]
+  fwrite(will.il.cvi.pct.toxpi.cat,
+         paste0("Will.IL.","CVI-pct-cat-",gsub(": ","-",onecat),".csv"))
+}
+
+cvi.pct.toxpi <- fread("CVI-pct-comb.csv",
+                       keepLeadingZeros = TRUE)
+will.il.cvi.pct.toxpi <- cvi.pct.toxpi[indx.will.il,]
+fwrite(will.il.cvi.pct.toxpi,
+       paste0("Will.IL.","CVI-pct-comb.csv"))
