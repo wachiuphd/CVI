302e944adb3febf7fadcca7bbdca73c4b7f87b66
wachiuphd <34288126+wachiuphd@users.noreply.github.com>
2022-04-27 11:27:50 -0400

    Update Merge_shapefile_CVI.R

--- Merge_shapefile_CVI.R
+++ Merge_shapefile_CVI.R
@@ -1,5 +1,13 @@
 # Merge ToxPi Scores and Data into 2010 shapefile tracts
 library(sp)
 library(raster)
-library(rgdal) ## Problem installing
+library(rgdal) ## use install.packages(c("rgdal"),repos="https://mac.R-project.org")
 p <- shapefile(file.path("Data","2010 Tracts","2010tracts.shp"))
+
+# # Load CSV file
+cvi.df<-fread("CVI_data_current.csv",
+              keepLeadingZeros = TRUE)
+# # merge on common variable, here called 'key'
+# m <- merge(p, d, by.x = "GEOID10", by.y = "GEOID.Tract")
+# # save as shapefile again
+# shapefile(m, "path/merged.shp")
